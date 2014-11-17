76a139c0a92b64819f832ee6851e960a96cdb8d7
Christian Forster <ch.forster@gmail.com>
2014-11-17 15:33:19 -0500

    save id with every pose

--- src/bag_to_pose/bag_to_pose.py
+++ src/bag_to_pose/bag_to_pose.py
@@ -6,24 +6,27 @@ import argparse
 def extract(bagfile, pose_topic, out_filename):
     n = 0
     f = open(out_filename, 'w')
-    f.write('# timestamp tx ty tz qx qy qz qz\n')    
+    f.write('# id timestamp tx ty tz qx qy qz qz\n')    
     with rosbag.Bag(bagfile, 'r') as bag:
         for (topic, msg, ts) in bag.read_messages(topics=str(pose_topic)):
             if False:
-                f.write('%.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' % 
-                        (msg.header.stamp.to_sec(),
+                f.write('%d %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' % 
+                        (n, 
+                         msg.header.stamp.to_sec(),
                          msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z,
                          msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w))
             elif False:
                 # Geometry Message Pose
-                f.write('%.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' % 
-                        (msg.header.stamp.to_sec(),
+                f.write('%d %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' % 
+                        (n,
+                         msg.header.stamp.to_sec(),
                          msg.pose.position.x, msg.pose.position.y, msg.pose.position.z,
                          msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w))
             else:
                 # Transform Stamped
-                f.write('%.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' % 
-                        (msg.header.stamp.to_sec(),
+                f.write('%d %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' % 
+                        (n,
+                         msg.header.stamp.to_sec(),
                          msg.transform.translation.x, msg.transform.translation.y, msg.transform.translation.z,
                          msg.transform.rotation.x, msg.transform.rotation.y, msg.transform.rotation.z, msg.transform.rotation.w))
             n += 1
